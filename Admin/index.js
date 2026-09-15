d459337dcb6618d6bb5eab46c0b413709f2a0bb4
Apple <toruleng@gmail.com>
2026-09-15 13:08:38 +0700

    been committing but it wont load

--- Admin/index.js
+++ Admin/index.js
@@ -12,5 +12,9 @@ app.get("/", (req,res) => {
     res.send("Hello Admin")
 })
 
+app.get("/ad", (res,res) => {
+    res.send("Send some requests")
+})
+
 // START THE EXPRESS SERVER. 5000 is the PORT NUMBER
 app.listen(5001, () => console.log('EXPRESS Server Started at Port No: 5001'));
